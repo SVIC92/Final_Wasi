5bc746f361789f7b399c6e4a44cd9df0cff0d43a
SVIC92 <victorjosesanchezlaureano@gmail.com>
2024-11-20 19:17:57 -0500

    commit

--- Proyecto_Wasi_Final/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proyecto_Wasi_Final.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
